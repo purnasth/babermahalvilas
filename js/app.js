de701a05f898ac05b7e15464a42744ad5bc2fa3e
Purna Shrestha <crush9918@gmail.com>
2023-12-17 02:29:48 +0545

    Create room page

--- js/app.js
+++ js/app.js
@@ -97,3 +97,24 @@ $(document).ready(function () {
     touchMove: true, // Enable touch move
   });
 });
+
+// rooms carousel
+document.addEventListener("DOMContentLoaded", function () {
+  new Glide(".glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+    focusAt: "center",
+    autoplay: 5000,
+    hoverpause: true,
+    gap: 0,
+    breakpoints: {
+      768: {
+        perView: 1,
+      },
+      1024: {
+        perView: 1,
+      },
+    },
+  }).mount();
+});
