ee25ed8658d9abbb6572b9b5e62e4641879a58cf
Purna Shrestha <crush9918@gmail.com>
2023-12-17 22:56:44 +0545

    Create testimonials section and optimize lightroom

--- js/app.js
+++ js/app.js
@@ -99,8 +99,48 @@ $(document).ready(function () {
 });
 
 // rooms carousel
+// document.addEventListener("DOMContentLoaded", function () {
+//   new Glide(".glide", {
+//     type: "carousel",
+//     startAt: 0,
+//     perView: 1,
+//     focusAt: "center",
+//     autoplay: 5000,
+//     hoverpause: true,
+//     gap: 0,
+//     breakpoints: {
+//       768: {
+//         perView: 1,
+//       },
+//       1024: {
+//         perView: 1,
+//       },
+//     },
+//   }).mount();
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
-  new Glide(".glide", {
+  // Initialize Glide for the rooms section
+  new Glide(".rooms-glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+    focusAt: "center",
+    autoplay: 5000,
+    hoverpause: true,
+    gap: 0,
+    breakpoints: {
+      768: {
+        perView: 1,
+      },
+      1024: {
+        perView: 1,
+      },
+    },
+  }).mount();
+
+  // Initialize Glide for the testimonials section
+  new Glide(".testimonials-glide", {
     type: "carousel",
     startAt: 0,
     perView: 1,
