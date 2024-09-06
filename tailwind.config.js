fd3754f9d51a89c24ee80840b4de579ae3a0703e
Purna Shrestha <crush9918@gmail.com>
2024-09-06 17:33:31 +0545

    feat: Update and complete final update

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
         // "light-coffee-clr": "#FFE9D2",
         // "white-clr": "#E1F7EB",
         // "yellow-clr": "#FFDE59",
+        dark: "#111111",
       },
     },
   },
