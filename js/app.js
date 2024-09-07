d203ae181057158326dd7720a82439864fa73156
Purna Shrestha <crush9918@gmail.com>
2024-09-07 21:13:40 +0545

    feat: Update hero slider

--- js/app.js
+++ js/app.js
@@ -69,6 +69,25 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 $(document).ready(function () {
+  $(".slider-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    animateOut: "fadeOut",
+    smartSpeed: 1000,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="fa-solid fa-chevron-left text-base bg-coffee-700/30 text-white px-4 py-2 rounded-full opacity-60 hover:opacity-100 transition-all duration-150 ease-in-out border border-white"></i>',
+      '<i class="fa-solid fa-chevron-right text-base bg-coffee-700/30 text-white px-4 py-2 rounded-full opacity-60 hover:opacity-100 transition-all duration-150 ease-in-out border border-coffee-400"></i>',
+      // '<i class="text-2xl mx-4 text-coffee-400">&#8592;</i>',
+      // '<i class="text-2xl mx-4 text-coffee-400">&#8594;</i>',
+    ],
+  });
+});
+$(document).ready(function () {
   $(".rooms-carousel").owlCarousel({
     items: 1,
     loop: true,
