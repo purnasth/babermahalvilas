90327faae64550ee0ac16ca59df435e8c1cc261c
Purna Shrestha <crush9918@gmail.com>
2024-09-16 19:04:49 +0545

    fix: Update the container size add 2xl

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,26 @@ module.exports = {
       fontSize: {
         inherit: "inherit",
       },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        // "2xl": "1536px",
+        "2xl": "1600px",
+        "3xl": "1920px",
+      },
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1600px",
+        },
+      },
     },
   },
   plugins: [],
